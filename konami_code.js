930650efed1c93c4b10e786ae11b6fe1b4b83ce0
John Kalinauskas <jkalinauskas@live.com>
2018-03-27 16:32:10 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,1 +1,28 @@
+function init() {
+   // Write your JavaScript code inside the init() function	   // Write your JavaScript code inside the init() function
+ 	 
+} 	  const body = document.body
 
+  let index = 0;
+ 
+  // This is the function that would be invoked by the event listener.
+  function onKeyDownHandler(e) {
+   const key = parseInt(e.detail || e.which);
+ 
+    if (key === code[index]) {
+     index++;
+  
+      if (index === code.length) {
+       alert("You Konami cheater, you! Well done!");
+ 
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  }
+  
+  body.addEventListener('keydown', onKeyDownHandler);
+}
+
+init() 
