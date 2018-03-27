a87466a1787c0727512d95dad30fcff41679019b
John Kalinauskas <jkalinauskas@live.com>
2018-03-27 16:30:49 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,6 @@
  const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
  	 
- function init() {	 function init() {
+ 	 function init() {
   // Write your JavaScript code inside the init() function	+  let index=0;
   document.body.addEventListener('keydown', function(e) {
    const key = parseInt(e.detail || e.which);
@@ -9,5 +9,5 @@
     }
      if (index === code.length) {alert("Good work, Narwhal!")
      index=0
-  }}
-})
+  }
+}})
