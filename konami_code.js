f7a215b2caf41d6bbad1a04e7d54eb5ec096a14e
John Kalinauskas <jkalinauskas@live.com>
2018-03-27 16:39:00 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,22 +1,3 @@
-function init(e) {
-  // Write your JavaScript code inside the init() function	+  let index = 0;
- let documentBody = document.querySelector('body');
-} 	  
- documentBody.addEventListener('keydown', function(e) {
-   const key = parseInt(e.location);
-    
-    if (key === code[index]) {
-    index++;
-    
-    if (index === code.length) {
-      alert("Hurray!");
- 
-      index = 0;
-    }
-  } else {
-    index = 0;
- } });
-}
-init();
+
 
 
